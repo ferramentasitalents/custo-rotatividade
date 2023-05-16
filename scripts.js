15007a2eb88a1e02508b00adb9d70568e26b3893
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 16:47:09 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-
+function calculateCost() {
+  const custoSubstituicao = parseFloat(document.getElementById("custo-substituicao").value);
+  const numFuncionarios = parseFloat(document.getElementById("num-funcionarios").value);
+  const cost = (custoSubstituicao / numFuncionarios) * 100;
+  document.getElementById("result").innerHTML = `Custo de Rotatividade: ${cost.toFixed(2)}%`;
+}
